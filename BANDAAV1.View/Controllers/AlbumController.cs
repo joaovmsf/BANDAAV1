369fbdc0650f4c9b7ec5ce8d7f6ab5063feeb939
joaov <joaov@DESKTOP-QTC7JNL>
2022-05-01 16:20:35 -0300

    Add project files.

---
+++ BANDAAV1.View/Controllers/AlbumController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BANDAAV1.Model;
+
+namespace BANDAAV1.View.Controllers
+{
+    public class AlbumController : Controller
+    {
+        // GET: AlbumController
+        BANDASContext db;
+        public AlbumController()
+        {
+            db = new BANDASContext();
+        }
+        public ActionResult Index()
+        {
+            List<Tbalbum> oLista = db.Tbalbum.ToList();
+            return View(oLista);
+        }
+
+        // GET: AlbumController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AlbumController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AlbumController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Tbalbum oAlb)
+        {
+            db.Tbalbum.Add(oAlb);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: AlbumController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Tbalbum oAlb = db.Tbalbum.Find(id);
+            return View(oAlb);
+        }
+
+        // POST: AlbumController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Tbalbum oAlb)
+        {
+            var oAlbBanco = db.Tbalbum.Find(id);
+            oAlbBanco.AlbumNome = oAlb.AlbumNome;
+            db.Entry(oAlbBanco).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: AlbumController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AlbumController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BANDAAV1.View/Controllers/BandaController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BANDAAV1.Model;
+
+namespace BANDAAV1.View.Controllers
+{
+    public class BandaController : Controller
+    {
+        // GET: BandaController
+        BANDASContext db;
+        public BandaController()
+        {
+            db = new BANDASContext();
+        }
+        public ActionResult Index()
+        {
+            List<Tbbanda> oLista = db.Tbbanda.ToList(); 
+            return View(oLista);
+        }
+
+        // GET: BandaController/Details/5
+        public ActionResult Details(int id)
+        {
+            Tbbanda oBan = db.Tbbanda.Find(id);
+            return View(oBan);
+        }
+
+        // GET: BandaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BandaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Tbbanda oBan)
+        {
+            db.Tbbanda.Add(oBan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: BandaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Tbbanda oBan = db.Tbbanda.Find(id);
+            return View(oBan);
+        }
+
+        // POST: BandaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Tbbanda oBan)
+        {
+            var oBanBanco = db.Tbbanda.Find(id);
+            oBanBanco.BandaNome = oBan.BandaNome;
+            oBanBanco.Bandagenero = oBan.Bandagenero;
+            db.Entry(oBanBanco).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: BandaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Tbbanda oBan = db.Tbbanda.Find(id);
+            return View(oBan);
+        }
+
+        // POST: BandaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Tbbanda oBan)
+        {
+            var oBanBanco = db.Tbbanda.Find(id);
+            oBanBanco.BandaNome = oBan.BandaNome;
+            oBanBanco.Bandagenero = oBan.Bandagenero;
+            db.Entry(oBanBanco).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
